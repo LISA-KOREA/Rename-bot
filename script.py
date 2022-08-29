cff75eec717485126eea4c2efd28a78e692c8a9d
Lisa <106958298+LISA-KOREA@users.noreply.github.com>
2022-08-30 01:29:06 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,1 +1,12 @@
-# soon
+class Translation(object):
+
+    START_TEXT = """🤗 Hi {},
+
+`I'm Telegram Rename Bot`
+
+**You can rename any Telegram Document, using this bot! With Permenant Thumbnail**
+
+Check /help
+"""
+
+    HELP_TEXT = """How to use this bot"""
