fae110df47bb2322f3575081430a7cc1785b2617
Lisa <106958298+LISA-KOREA@users.noreply.github.com>
2022-09-02 22:42:25 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,17 @@
+import os
+
+class Config(object):
+
+    TOKEN = os.environ.get("TOKEN", "")
+
+    API_ID = int(os.environ.get("API_ID", ""))
+
+    API_HASH = os.environ.get("API_HASH", "")
+
+    DB_NAME = os.environ.get("DB_NAME","")
+
+    DB_URL = os.environ.get("DB_URL","")
+
+    UPDATE_GROUP = os.environ.get("UPDATE_GROUP", "")
+    
+    UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", "")
